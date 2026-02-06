172fa1e6b32bc8bf9b3a5785d8ea39fe8cd70cab
Saif Abdelrazek (A8K) <git@saifabdelrazek.com>
2026-02-06 00:01:49 +0000

    feat: configure sitemap integration and update layout meta tags

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
+import sitemap from "@astrojs/sitemap";
+import { siteConfig } from "./src/data/constants";
+
 // https://astro.build/config
 export default defineConfig({
+  site: siteConfig.url,
   vite: {
     plugins: [tailwindcss()],
   },
+  integrations: [sitemap()],
 });
