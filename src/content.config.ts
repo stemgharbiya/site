6e8092baa00ecc736153cfb4f83dd4201879037c
Amr Loksha <amrloksha151@proton.me>
2026-02-11 11:08:05 +0200

    feat: adding-alumni-component

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,14 +3,32 @@ import { file } from 'astro/loaders';
 import { z } from 'zod';
 
 const alumniCollection = defineCollection({
-    loader: file('./data/alumni.json'),
-    schema: ({image}) => z.object({
-        alumni: z.array(
+    loader: file('./src/data/alumni.json'),
+    schema: ({ image }) => z.object({
+        id: z.string(),
+        year: z.number(),
+        students: z.array(
             z.object({
-                id: z.number(),
                 name: z.string(),
                 image: image(),
-                graduationYear: z.number()
+                class: z.string(),
+                classRank: z.string().optional(),
+                nationalRank: z.string().optional(),
+                socialMedia: z.object({
+                    cv: z.string().optional(),
+                    website: z.string().optional(),
+                    email: z.string().optional(),
+                    linkedin: z.string().optional(),
+                    facebook: z.string().optional(),
+                    instagram: z.string().optional()
+                }).optional(),
+                university: z.object({
+                    logo: image(),
+                    name: z.string(),
+                    classYear: z.number(),
+                    major: z.string().optional(),
+                    financialAid: z.string().optional()
+                })
             })
         )
     })
