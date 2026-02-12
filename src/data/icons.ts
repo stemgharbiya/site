29f4a6eb936e3315cf2c8ee0c6817c10f2ff62d0
Saif Abdelrazek (A8K) <git@saifabdelrazek.com>
2026-02-12 17:00:47 +0000

    feat: add astro-icon and replace inline svgs with Icon components

---
+++ src/data/icons.ts
@@ -0,0 +1,9 @@
+export const ICONS = {
+  github: "simple-icons:github",
+  linkedin: "simple-icons:linkedin",
+  facebook: "simple-icons:facebook",
+  sun: "mdi:weather-sunny",
+  moon: "mdi:weather-night",
+  mapMarker: "mdi:map-marker",
+  home: "mdi:home",
+};
