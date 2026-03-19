bc8677e536e1b50764e93ea6696af09ec4884bc3
saif-abdelrazek <git@saifabdelrazek.com>
2026-03-19 13:53:50 +0200

    chore(commitlint): enforce conventional commits via husky hook

---
+++ commitlint.config.mjs
@@ -0,0 +1,8 @@
+export default {
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "header-max-length": [2, "always", 100],
+    "body-max-line-length": [1, "always", 100],
+    "footer-max-line-length": [1, "always", 100],
+  },
+};
